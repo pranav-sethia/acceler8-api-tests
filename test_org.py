d1d51fffbcd5443879f109e2b5dd42d94bc08bf8
kaustubh <kaustubh@tccapita.com>
2025-07-30 21:53:42 +0530

    add employee

--- test_org.py
+++ test_org.py
@@ -3,7 +3,7 @@ import pytest
 import json
 
 API_HOST = "https://dev3.api.theacceler8.com"
-AUTH_TOKEN = "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJkMWU2Njg2Zi1hMTE3LTRmOTMtOTY5Yy00MTU0Mjg1OWJlOWQiLCJvcmdfaWQiOiJmNTc5MmQxYS1hMGQ5LTQ5ODctODQ0OS1kMzE5ZGU1MjlmNGUiLCJpYXQiOjE3NTIxNzMyMzAsImV4cCI6MTc1MjE4NDAzMH0.QHXkbj9ZS1btbGY3mP3Hmk8V4cVqoJLiE8OB0U0K9F7HmAl-9NY3krEvR8_WdDNq"
+AUTH_TOKEN = "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiI0Y2I1ZGY2Zi1mNDJlLTRjODgtYWI1Ni1iMzcxNmMzMTgxYjAiLCJvcmdfaWQiOiJmNTc5MmQxYS1hMGQ5LTQ5ODctODQ0OS1kMzE5ZGU1MjlmNGUiLCJpYXQiOjE3NTI2NDkwODMsImV4cCI6MTc1MjY1OTg4M30.U-xeUQ2ENV9OsNENHT43mOUA6zkjH_mdCHaD_uwmpLZoduA65sXlkajRcHRnovR6"
 BASE_URL = f"{API_HOST}/backend/v1/organisation"
 
 @pytest.fixture(scope="module")
@@ -20,6 +20,7 @@ def created_org():
     response = requests.post(BASE_URL, json=body, headers=headers)
     assert response.status_code == 200, f"Request failed: {json.dumps(response.json(), indent=2)}"
     org_id = response.json()["data"]["id"]
+
     yield org_id
     requests.delete(f"{BASE_URL}/{org_id}", headers=headers)
 
